dad5c4ec014b8c30592b9877b0ccf6d688b4e241
Aries Dmello <dmelloaries@gmail.com>
2024-03-19 20:57:35 +0530

    Added UI Changes

---
+++ UI/Home.py
@@ -0,0 +1,551 @@
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
+
+import tkinter as tk
+import random
+
+# Function to generate random gibberish
+def generate_gibberish():
+    gibberish = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=random.randint(5, 20)))
+    return gibberish
+
+# Function to update the label with random gibberish
+def update_label():
+    gibberish_text.set(generate_gibberish())
+
+# Function to change the background color
+def change_bg_color():
+    colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33F3', '#F3FF33']
+    bg_color = random.choice(colors)
+    root.configure(background=bg_color)
+
+# Creating the main window
+root = tk.Tk()
+root.title("Python Homepage")
+root.geometry("500x400")
+
+# Creating a frame to hold the content
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
+# Creating a label to display random gibberish
+gibberish_text = tk.StringVar()
+gibberish_label = tk.Label(frame, textvariable=gibberish_text, font=("Arial", 16), wraplength=450)
+gibberish_label.pack(pady=10)
+
+# Creating a button to generate new random gibberish
+generate_button = tk.Button(frame, text="Generate Gibberish", command=update_label)
+generate_button.pack()
+
+# Creating a button to change background color
+color_button = tk.Button(frame, text="Change Background Color", command=change_bg_color)
+color_button.pack()
+
+# Initial generation of gibberish
+update_label()
+
+# Running the Tkinter event loop
+root.mainloop()
---
+++ UI/login.py
@@ -0,0 +1,182 @@
+class VirtualMachine(object):
+   ## [... snip ...]
+
+    ## Stack manipulation
+
+    def byte_LOAD_CONST(self, const):
+        self.push(const)
+
+    def byte_POP_TOP(self):
+        self.pop()
+
+    ## Names
+    def byte_LOAD_NAME(self, name):
+        frame = self.frame
+        if name in frame.f_locals:
+            val = frame.f_locals[name]
+        elif name in frame.f_globals:
+            val = frame.f_globals[name]
+        elif name in frame.f_builtins:
+            val = frame.f_builtins[name]
+        else:
+            raise NameError("name '%s' is not defined" % name)
+        self.push(val)
+
+    def byte_STORE_NAME(self, name):
+        self.frame.f_locals[name] = self.pop()
+
+    def byte_LOAD_FAST(self, name):
+        if name in self.frame.f_locals:
+            val = self.frame.f_locals[name]
+        else:
+            raise UnboundLocalError(
+                "local variable '%s' referenced before assignment" % name
+            )
+        self.push(val)
+
+    def byte_STORE_FAST(self, name):
+        self.frame.f_locals[name] = self.pop()
+
+    def byte_LOAD_GLOBAL(self, name):
+        f = self.frame
+        if name in f.f_globals:
+            val = f.f_globals[name]
+        elif name in f.f_builtins:
+            val = f.f_builtins[name]
+        else:
+            raise NameError("global name '%s' is not defined" % name)
+        self.push(val)
+
+    ## Operators
+
+    BINARY_OPERATORS = {
+        'POWER':    pow,
+        'MULTIPLY': operator.mul,
+        'FLOOR_DIVIDE': operator.floordiv,
+        'TRUE_DIVIDE':  operator.truediv,
+        'MODULO':   operator.mod,
+        'ADD':      operator.add,
+        'SUBTRACT': operator.sub,
+        'SUBSCR':   operator.getitem,
+        'LSHIFT':   operator.lshift,
+        'RSHIFT':   operator.rshift,
+        'AND':      operator.and_,
+        'XOR':      operator.xor,
+        'OR':       operator.or_,
+    }
+
+    def binaryOperator(self, op):
+        x, y = self.popn(2)
+        self.push(self.BINARY_OPERATORS[op](x, y))
+
+    COMPARE_OPERATORS = [
+        operator.lt,
+        operator.le,
+        operator.eq,
+        operator.ne,
+        operator.gt,
+        operator.ge,
+        lambda x, y: x in y,
+        lambda x, y: x not in y,
+        lambda x, y: x is y,
+        lambda x, y: x is not y,
+        lambda x, y: issubclass(x, Exception) and issubclass(x, y),
+    ]
+
+    def byte_COMPARE_OP(self, opnum):
+        x, y = self.popn(2)
+        self.push(self.COMPARE_OPERATORS[opnum](x, y))
+
+    ## Attributes and indexing
+
+    def byte_LOAD_ATTR(self, attr):
+        obj = self.pop()
+        val = getattr(obj, attr)
+        self.push(val)
+
+    def byte_STORE_ATTR(self, name):
+        val, obj = self.popn(2)
+        setattr(obj, name, val)
+
+    ## Building
+
+    def byte_BUILD_LIST(self, count):
+        elts = self.popn(count)
+        self.push(elts)
+
+    def byte_BUILD_MAP(self, size):
+        self.push({})
+
+    def byte_STORE_MAP(self):
+        the_map, val, key = self.popn(3)
+        the_map[key] = val
+        self.push(the_map)
+
+    def byte_LIST_APPEND(self, count):
+        val = self.pop()
+        the_list = self.frame.stack[-count] # peek
+        the_list.append(val)
+
+    ## Jumps
+
+    def byte_JUMP_FORWARD(self, jump):
+        self.jump(jump)
+
+    def byte_JUMP_ABSOLUTE(self, jump):
+        self.jump(jump)
+
+    def byte_POP_JUMP_IF_TRUE(self, jump):
+        val = self.pop()
+        if val:
+            self.jump(jump)
+
+    def byte_POP_JUMP_IF_FALSE(self, jump):
+        val = self.pop()
+        if not val:
+            self.jump(jump)
+
+    ## Blocks
+
+    def byte_SETUP_LOOP(self, dest):
+        self.push_block('loop', dest)
+
+    def byte_GET_ITER(self):
+        self.push(iter(self.pop()))
+
+    def byte_FOR_ITER(self, jump):
+        iterobj = self.top()
+        try:
+            v = next(iterobj)
+            self.push(v)
+        except StopIteration:
+            self.pop()
+            self.jump(jump)
+
+    def byte_BREAK_LOOP(self):
+        return 'break'
+
+    def byte_POP_BLOCK(self):
+        self.pop_block()
+
+    ## Functions
+
+    def byte_MAKE_FUNCTION(self, argc):
+        name = self.pop()
+        code = self.pop()
+        defaults = self.popn(argc)
+        globs = self.frame.f_globals
+        fn = Function(name, code, globs, defaults, None, self)
+        self.push(fn)
+
+    def byte_CALL_FUNCTION(self, arg):
+        lenKw, lenPos = divmod(arg, 256) # KWargs not supported here
+        posargs = self.popn(lenPos)
+
+        func = self.pop()
+        frame = self.frame
+        retval = func(*posargs)
+        self.push(retval)
+
+    def byte_RETURN_VALUE(self):
+        self.return_value = self.pop()
+        return "return"
